1b60fae43ebfac0b508922b2f7da9b6c7e823423
Mike424-star <74331713+Mike424-star@users.noreply.github.com>
2021-01-31 13:33:50 +0100

    Add files via upload

---
+++ Guess the number/Guess the number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Guess_the_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Random().Next(1, 10);
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("The secret number is (Between 1 to 10): ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You Won!");
+                    return;
+                }
+
+            }
+            Console.WriteLine("You Lost");
+        }
+
+    }
+}
